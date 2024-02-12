cdc06f4260b44a7ad19ef0401fff16c144ef4c22
Daniela <dtricolici12345@gmail.com>
2024-02-12 14:51:24 +0100

    added express-quests 2

---
+++ src/controllers/userControllers.js
@@ -0,0 +1,39 @@
+
+const users = [
+    /* ... */
+  ];
+  
+  const database = require("../../database");
+
+const getUsers = (req, res) => {
+    database
+      .query("SELECT * FROM users")
+      .then(([users]) => {
+        res.status(200).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+  
+  const getUserById = (req, res) => {
+    const id = parseInt(req.params.id);
+    database
+      .query("SELECT * FROM users WHERE id = ?", [id])
+      .then(([users]) => {
+        if (users.length > 0) {
+          res.status(200).json(users[0]);
+        } else {
+          res.sendStatus(404);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+module.exports = {
+    getUsers,
+    getUserById,
+};
